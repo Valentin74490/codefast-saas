4bdc38bc9213fa15dd3f12b1d4fe9e5225f6f9f1
Valentin DUCRETTET <valentin.ducrettet@partagetabanque.fr>
2025-03-20 17:39:35 +0100

    intégration partiel de la réponse API lors de la création d'un board

--- components/FormNewBoard.js
+++ components/FormNewBoard.js
@@ -14,7 +14,7 @@ const FormNewBoard = () => {
     setIsLoading(true);
 
     try {
-      await fetch("/api/board", {
+      const response = await fetch("/api/board", {
         method: "POST",
         body: JSON.stringify({
           name,
@@ -23,6 +23,9 @@ const FormNewBoard = () => {
           "content-type": "application/json",
         }
       });
+      const data = await response.json();
+      console.log(data);
+
 
     } catch (error) {
 
